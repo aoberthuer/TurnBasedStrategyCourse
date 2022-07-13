5ab82624458048c3c732678d260da9a5bb786fe6
Aloys <aloys.oberthuer@gmail.com>
2022-07-13 18:44:04 +0200

    GridSystem

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    public readonly struct GridPosition
+    {
+        public readonly int x;
+        public readonly int z;
+
+        public GridPosition(int x, int z)
+        {
+            this.x = x;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"x: { x }; z: { z }";
+        }
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Testing : MonoBehaviour
+    {
+        private GridSystem _gridSystem;
+
+        private void Start()
+        {
+            _gridSystem = new GridSystem(10, 10, 2f);
+        }
+
+        private void Update()
+        {
+            Debug.Log(_gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+        }
+    }
+}
