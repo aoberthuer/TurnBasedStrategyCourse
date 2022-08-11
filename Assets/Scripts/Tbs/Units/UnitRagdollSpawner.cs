ba9ebb0bb683717d2d16af50ce8989038e9bd5bd
Aloys <aloys.oberthuer@gmail.com>
2022-08-11 22:27:51 +0200

    Ragdoll

---
+++ Assets/Scripts/Tbs/Units/UnitRagdollSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace tbs.units
+{
+    public class UnitRagdollSpawner : MonoBehaviour
+    {
+        [SerializeField] private Transform ragdollPrefab;
+        [SerializeField] private Transform originalRootBone;
+
+
+        private HealthSystem healthSystem;
+
+        private void Awake()
+        {
+            healthSystem = GetComponent<HealthSystem>();
+
+            healthSystem.OnDead += HealthSystem_OnDead;
+        }
+
+        private void HealthSystem_OnDead()
+        {
+            Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+            UnitRagdoll unitRagdoll = ragdollTransform.GetComponent<UnitRagdoll>();
+            unitRagdoll.Setup(originalRootBone);
+        }
+
+    }
+}
