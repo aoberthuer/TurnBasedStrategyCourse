79ec044ad6c0746380fd942ece5779fbea8215b2
Aloys <aloys.oberthuer@gmail.com>
2022-08-14 16:59:21 +0200

    Unit World UI

---
+++ Assets/Scripts/Tbs/UI/UnitWorldUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using tbs.units;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace tbs.ui
+{
+    public class UnitWorldUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI actionPointsText;
+        [SerializeField] private Unit unit;
+        [SerializeField] private Image healthBarImage;
+        [SerializeField] private HealthSystem healthSystem;
+
+        private void Start()
+        {
+            Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+            healthSystem.OnDamaged += HealthSystem_OnDamaged;
+
+            UpdateActionPointsText();
+            UpdateHealthBar();
+        }
+
+        private void UpdateActionPointsText()
+        {
+            actionPointsText.text = unit.GetActionPoints().ToString();
+        }
+
+        private void Unit_OnAnyActionPointsChanged()
+        {
+            UpdateActionPointsText();
+        }
+
+        private void UpdateHealthBar()
+        {
+            healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
+        }
+
+        private void HealthSystem_OnDamaged()
+        {
+            UpdateHealthBar();
+        }
+
+        
+    }
+}
--- Assets/Scripts/Tbs/Units/HealthSystem.cs
+++ Assets/Scripts/Tbs/Units/HealthSystem.cs
@@ -6,12 +6,20 @@ namespace tbs.units
     public class HealthSystem : MonoBehaviour
     {
         public event Action OnDead;
+        public event Action OnDamaged;
 
         [SerializeField] private int health = 100;
+        private int _healthMax;
+
+        private void Awake()
+        {
+            _healthMax = health;
+        }
 
         public void Damage(int damageAmount)
         {
             health -= damageAmount;
+            OnDamaged?.Invoke();
 
             if (health < 0)
             {
@@ -30,6 +38,13 @@ namespace tbs.units
         {
             OnDead?.Invoke();
         }
+        
+        public float GetHealthNormalized()
+        {
+            return (float)health / _healthMax;
+        }
+
+
 
     }
 }
